baaf25597d15d3a0d09d64a296006f2f3784a573
Александр <70471514+GigantaMother@users.noreply.github.com>
2021-09-16 15:47:41 +0300

    Update main.cpp

--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include "Array.hpp"
+# include "Array.hpp"
 
-#define MAX_VAL 5
-#define RANDOM  10
-#define LEAKS  	1
+# define MAX_VAL	5
+# define RANDOM		10
+# define LEAKS		1
 
-#define DEFAULT		"\033[0m"
-#define BOLD		"\033[1m"
-#define UNDERLINE	"\033[4m"
-#define REVERS		"\033[7m"
-#define RED			"\033[31m"
-#define GREEN		"\033[32m"
-#define YELLOW		"\033[33m"
-#define BLUE		"\033[34m"
-#define	AQUA		"\033[36m"
+# define DEFAULT	"\033[0m"
+# define BOLD		"\033[1m"
+# define UNDERLINE	"\033[4m"
+# define REVERS		"\033[7m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define AQUA		"\033[36m"
 
 void print_revers(std::string str);
 void test_1();
